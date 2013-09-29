096dc276686b8b3d5b0ff3852aa8b7eeb031e6e0
GAURAV KUMAR <gauravkumar552@gmail.com>
2013-09-29 20:02:48 +0530

    using display variables

--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+var1="date"
+var2="watch sensors"
+var3="tail -f /var/log/syslog"
 
 tmux new-session -d -s SysWatch 'exec sys'
 
-tmux send-keys 'date' 'C-m'
+tmux send-keys "$var1" 'C-m'
 
 tmux select-window -t SysWatch:0
 tmux rename-window 'SYS'
 
 tmux split-window -h #'exec sys'
 
-tmux send-keys 'watch sensors' 'C-m'
+tmux send-keys "$var2" 'C-m'
 tmux split-window -v -t 0 #'exec sys'
 
-tmux send-keys 'tail -f /var/log/syslog' 'C-m'
+tmux send-keys "$var3" 'C-m'
 tmux split-window -v -t 1 #'exec sys'
 
 tmux -2 attach-session -t SysWatch
